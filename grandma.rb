ca29118b17992b16344f6eea03e4e6d8e81c1741
Sean Carter <scarter0313@gmail.com>
2019-05-08 01:36:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else phrase != phrase.upcase
+end
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elseif
+  else phrase
+  
   end
 end
